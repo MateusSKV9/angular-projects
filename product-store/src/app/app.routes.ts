240e3af5be62d327248ffafe05e6a7f1f291ad62
Mateus Santos <mateussilvanzg@gmail.com>
2024-10-11 19:56:33 -0300

    update project

--- product-store/src/app/app.routes.ts
+++ product-store/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: ListComponent,
+}
+];
