efdb4c2f4336a1248ba5b36eca7e87fed8eda8a0
Mateus Santos <mateussilvanzg@gmail.com>
2024-10-11 21:50:01 -0300

    update project

--- product-store/src/app/features/list/list.component.ts
+++ product-store/src/app/features/list/list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+products: any[] = [];
 
+  productsService = inject(ProductsService);
+
+  ngOnInit() {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
