0ff8a70ed4936799a3e9df8455c5acfc8fa9375c
Mateus Santos <mateussilvanzg@gmail.com>
2024-12-24 22:24:44 -0300

    correction

--- product-store/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListComponent } from './features/list/list.component';
-
-export const routes: Routes = [{
-    path: '',
-    component: ListComponent,
-}
-];
--- product-store/src/app/shared/interfaces/produtc.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface Product {
-     id: string;
-     title: string;
-}
