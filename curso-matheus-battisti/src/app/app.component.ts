f7554cf72399e9e6c4b18304eb9e52559823e0c8
Mateus Santos <mateussilvanzg@gmail.com>
2024-12-25 13:46:25 -0300

    conditional redering

--- curso-matheus-battisti/src/app/app.component.ts
+++ curso-matheus-battisti/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 import { DirectivesComponent } from "./components/directives/directives.component";
+import { IfRenderComponent } from "./components/if-render/if-render.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FirstComponentComponent, ParentDataComponent, DirectivesComponent],
+  imports: [FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
