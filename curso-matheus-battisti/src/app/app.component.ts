5bdb69f8d895897a7c36e3ce177669d4434c1b88
Mateus Santos <mateussilvanzg@gmail.com>
2024-12-25 12:23:05 -0300

    angular directives

--- curso-matheus-battisti/src/app/app.component.ts
+++ curso-matheus-battisti/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
+import { DirectivesComponent } from "./components/directives/directives.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FirstComponentComponent, ParentDataComponent],
+  imports: [FirstComponentComponent, ParentDataComponent, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ curso-matheus-battisti/src/app/components/directives/directives.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+  classes = ["green-title", "small-title"]
+}
