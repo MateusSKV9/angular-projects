0e3de385ccef81a7dd494f256d0b2d40b4579889
Mateus Santos <mateussilvanzg@gmail.com>
2024-12-25 11:22:56 -0300

    data sharing

--- curso-matheus-battisti/src/app/app.component.ts
+++ curso-matheus-battisti/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [FirstComponentComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'joaquim@gamil.com',
+    role: 'Admin'
+  }
+
   title = 'curso-matheus-battisti';
 }
---
+++ curso-matheus-battisti/src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent-data.component.html',
+  styleUrl: './parent-data.component.css'
+})
+export class ParentDataComponent {
+  @Input() name: string = '';
+  @Input() data!: {email: string, role: string};
+}
