8d4fcc437cc18bed55ab78ac086d476296966aa7
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-01 16:20:28 -0300

    initial settings

--- moments/src/app/app.routes.ts
+++ moments/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
