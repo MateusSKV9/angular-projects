732c125e922c7421d70ee7c9eea45816dad342b6
Mateus Santos <mateussilvanzg@gmail.com>
2025-01-02 19:36:34 -0300

    update home

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Moment } from '../../../../Moments';
+import { enverionment } from '../../../environments/environment';
+import { MomentService } from '../../../services/moment.service';
+import { Response } from '../../../../Response';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = enverionment.baseApiUrl;
 
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { enverionment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Moment } from '../../Moments';
+import { Response } from '../../Response';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +13,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
